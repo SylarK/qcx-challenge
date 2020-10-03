dcb9ea6ed9e05a37f556d21afc51006a8f55088c
SylarK <syst.lucasamado@gmail.com>
2020-10-03 17:31:17 -0300

    Prototipo Ok. Criacao de navbar

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const burguer = document.querySelector('.btn');
+const opcoes  = document.querySelectorAll('.opcoes');
+
+window.addEventListener('load', () => {
+    listenBurguer();
+})
+
+function listenBurguer(){
+    burguer.addEventListener('click', () => {
+        opcoes.forEach(opcao => {
+            opcao.classList.toggle('mostrar');
+            //console.log(opcao);
+        })
+    })
+}
+
